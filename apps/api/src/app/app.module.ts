3c439126742c3509d62ebb8138a33242d94500d0
Rik Kierkels <Kierkels.r@gmail.com>
2019-12-23 13:48:12 +0100

    configure the postgres ORM through the config service

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,15 +2,34 @@ import { Module } from '@nestjs/common';
 import { IdeasModule } from './ideas/ideas.module';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from '../config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ load: [configuration] }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => getDatabaseConfig(config),
+      inject: [ConfigService]
+    }),
     IdeasModule,
     AuthModule,
     SharedModule
   ]
 })
 export class AppModule {}
+
+const getDatabaseConfig = (config: ConfigService) => {
+  return {
+    type: config.get('database.type'),
+    host: config.get('database.host'),
+    port: config.get('database.port'),
+    username: config.get('database.username'),
+    password: config.get('database.password'),
+    database: config.get('database.name'),
+    entities: ['dist/**/*.entity{.ts,.js}'],
+    synchronize: config.get('database.synchronize')
+  };
+};
--- apps/api/src/config/configuration.ts
+++ apps/api/src/config/configuration.ts
@@ -3,5 +3,14 @@ export default () => ({
   auth: {
     audience: process.env.AUTH0_AUDIENCE,
     domain: process.env.AUTH0_DOMAIN
+  },
+  database: {
+    type: process.env.DATABASE_TYPE,
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    name: process.env.DATABASE_NAME,
+    synchronize: process.env.DATABASE_SYNCHRONIZE === 'true'
   }
 });

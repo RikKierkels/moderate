92a8a8a996d099dcc22dc1eae16c9f16d5a6f06f
Rik Kierkels <Kierkels.r@gmail.com>
2019-12-23 21:09:37 +0100

    define a more structured public interface for the idea model

--- libs/api-interfaces/src/lib/idea.ts
+++ libs/api-interfaces/src/lib/idea.ts
@@ -1,28 +1,64 @@
 import { Message, Tag } from '@moderate/api-interfaces';
+import {
+  ArrayNotEmpty,
+  ArrayUnique,
+  IsInt,
+  IsNotEmpty,
+  IsString,
+  Max,
+  Min
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class Idea {
+  @ApiProperty()
   readonly id: number;
+
+  @ApiProperty()
   readonly title: string;
+
+  @ApiProperty()
   readonly description: string;
+
+  @ApiProperty()
   readonly difficulty: number;
+
+  @ApiProperty()
   readonly tags: Tag[];
+
+  @ApiProperty()
   readonly replies: Message[];
+
+  @ApiProperty()
   readonly authorId: string;
 }
 
 export class IdeaCreateDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
   readonly title: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
+
+  @ApiProperty()
+  @IsInt()
+  @Min(1)
+  @Max(5)
   readonly difficulty: number;
+
+  @ApiProperty()
+  @ArrayNotEmpty({ each: true })
+  @ArrayUnique({ each: true })
   readonly tags: Tag[];
-  readonly authorId: string;
 }
 
-export class IdeaUpdateDto {
+export class IdeaUpdateDto extends IdeaCreateDto {
+  @ApiProperty()
+  @IsInt()
+  @IsNotEmpty()
   readonly id: number;
-  readonly title: string;
-  readonly description: string;
-  readonly difficulty: number;
-  readonly tags: Tag[];
-  readonly authorId: string;
 }

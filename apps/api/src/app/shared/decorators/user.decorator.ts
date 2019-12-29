21300ca653e330c53e3341861d8fa144b71fc7a7
Rik Kierkels <Kierkels.r@gmail.com>
2019-12-29 10:25:28 +0100

    throw an exception if the userid is missing from an authorized request

--- apps/api/src/app/shared/decorators/user.decorator.ts
+++ apps/api/src/app/shared/decorators/user.decorator.ts
@@ -1,5 +1,9 @@
-import { createParamDecorator } from '@nestjs/common';
+import { BadRequestException, createParamDecorator } from '@nestjs/common';
 
 export const UserId = createParamDecorator((data, req) => {
+  if (!req.user || !req.user.sub) {
+    return new BadRequestException('UserId is missing from request.');
+  }
+
   return req.user.sub;
 });

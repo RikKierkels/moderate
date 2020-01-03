4a7e6a5e04673a35a8553158911ffe5013bff84d
Rik Kierkels <Kierkels.r@gmail.com>
2020-01-03 17:38:32 +0100

    fix a typo in the author guard that prevents authors from editting ideas

--- apps/api/src/app/shared/guards/is-author.guard.ts
+++ apps/api/src/app/shared/guards/is-author.guard.ts
@@ -1,7 +1,7 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { IdeaService } from '../../idea/idea.service';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class IsAuthorGuard implements CanActivate {
@@ -12,6 +12,6 @@ export class IsAuthorGuard implements CanActivate {
 
     return this.ideaService
       .find$(params.id)
-      .pipe(map(idea => idea.id === user.sub));
+      .pipe(map(idea => idea.author.id === user.sub));
   }
 }

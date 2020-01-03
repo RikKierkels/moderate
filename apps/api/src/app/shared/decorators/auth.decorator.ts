f0d9e59a012a24c671a6fb060aa9ad7cf1ad512e
Rik Kierkels <Kierkels.r@gmail.com>
2020-01-03 15:52:03 +0100

    extend the auth decorator so additional guards can be used

--- apps/api/src/app/shared/decorators/auth.decorator.ts
+++ apps/api/src/app/shared/decorators/auth.decorator.ts
@@ -1,12 +1,12 @@
 import { applyDecorators } from './apply-decorators';
-import { UseGuards } from '@nestjs/common';
+import { CanActivate, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiBearerAuth, ApiUnauthorizedResponse } from '@nestjs/swagger';
 
-export function Auth() {
+export function Auth(...additionalGuards: (CanActivate | Function)[]) {
   return applyDecorators(
     ApiBearerAuth(),
     ApiUnauthorizedResponse({ description: 'Unauthorized"' }),
-    UseGuards(AuthGuard('jwt'))
+    UseGuards(AuthGuard('jwt'), ...additionalGuards)
   );
 }

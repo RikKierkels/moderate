d97b85b4ae34b63c8c323d8f88cb5f3f0a6dcdd0
Rik Kierkels <Kierkels.r@gmail.com>
2020-01-06 19:16:56 +0100

    add a message controller

--- apps/api/src/app/message/message.module.ts
+++ apps/api/src/app/message/message.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MessageEntity } from '../database/database-entities';
 import { MessageService } from './message.service';
+import { MessageController } from './message.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([MessageEntity])],
-  providers: [MessageService]
+  providers: [MessageService],
+  controllers: [MessageController]
 })
 export class MessageModule {}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -19,6 +19,7 @@ async function bootstrap() {
       .setVersion('1.0')
       .addTag('Idea')
       .addTag('Tag')
+      .addTag('Message')
       .addBearerAuth()
       .build();
     const document = SwaggerModule.createDocument(app, options);

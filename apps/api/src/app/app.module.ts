1a50d08a0f58f9489bd03c702a785c4633c715e3
Rik Kierkels <Kierkels.r@gmail.com>
2020-01-06 21:58:17 +0100

    add an in memory cache for all GET endpoints

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { Module } from '@nestjs/common';
+import { CacheInterceptor, CacheModule, Module } from '@nestjs/common';
 import { IdeaModule } from './idea/idea.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import configuration from '../config/configuration';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ load: [configuration] }),
+    CacheModule.register(),
     DatabaseModule,
     IdeaModule,
     AuthModule,
     UserModule
+  ],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor
+    }
   ]
 })
 export class AppModule {}

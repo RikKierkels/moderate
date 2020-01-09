729aeed3dec38d2fabfdfab2c6fdbcce473682b0
Rik Kierkels <Kierkels.r@gmail.com>
2020-01-09 23:00:12 +0100

    add tests for the tag controller

--- apps/api/src/app/tag/tag.controller.spec.ts
+++ apps/api/src/app/tag/tag.controller.spec.ts
@@ -1,18 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TagController } from './tag.controller';
+import { TagService } from './tag.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { TagEntity } from '../database/database-entities';
+import { of } from 'rxjs';
+import { TagDto } from './tag.model';
+
+const tagsForTest: TagEntity[] = [{ id: '1', color: '#000000', name: 'Jest' }];
 
 describe('Tag Controller', () => {
   let controller: TagController;
+  let service: TagService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [TagController]
+      controllers: [TagController],
+      providers: [
+        TagService,
+        {
+          provide: getRepositoryToken(TagEntity),
+          useValue: null
+        }
+      ]
     }).compile();
 
     controller = module.get<TagController>(TagController);
+    service = module.get<TagService>(TagService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('finding all tags', () => {
+    it('should map found tags to dtos', () => {
+      jest.spyOn(service, 'findAll$').mockReturnValueOnce(of(tagsForTest));
+
+      controller.findAll().subscribe(tags => {
+        expect(tags instanceof TagDto).toBeTruthy();
+      });
+    });
   });
 });

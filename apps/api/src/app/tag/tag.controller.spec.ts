849cfe7c27ac4e57340943b2a7b91bb82a8d8080
Rik Kierkels <Kierkels.r@gmail.com>
2020-01-12 11:35:43 +0100

    simplify the tag controller tests by mocking the service

--- apps/api/src/app/tag/tag.controller.spec.ts
+++ apps/api/src/app/tag/tag.controller.spec.ts
@@ -1,37 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TagController } from './tag.controller';
 import { TagService } from './tag.service';
-import { getRepositoryToken } from '@nestjs/typeorm';
 import { TagEntity } from '../database/database-entities';
 import { of } from 'rxjs';
 import { TagDto } from './tag.model';
 
+jest.mock('./tag.service');
 const tagEntities: TagEntity[] = [{ id: '1', color: '#000000', name: 'Jest' }];
 
 describe('Tag Controller', () => {
   let controller: TagController;
-  let service: TagService;
+  let service: jest.Mocked<TagService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TagController],
-      providers: [
-        TagService,
-        {
-          provide: getRepositoryToken(TagEntity),
-          useValue: null
-        }
-      ]
+      providers: [TagService]
     }).compile();
 
-    controller = module.get<TagController>(TagController);
-    service = module.get<TagService>(TagService);
+    controller = module.get(TagController);
+    service = module.get(TagService);
   });
 
   describe('finding all tags', () => {
-    it('should map found tags to dtos', () => {
-      jest.spyOn(service, 'findAll$').mockReturnValueOnce(of(tagEntities));
+    beforeEach(() => {
+      service.findAll$.mockReturnValueOnce(of(tagEntities));
+    });
 
+    it('should map found tags to dtos', () => {
       controller.findAll().subscribe(tags => {
         expect(tags instanceof TagDto).toBeTruthy();
       });

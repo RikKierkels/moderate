9f3795e8e73bf03fa3bffb78d53bdd44e747238f
Rik Kierkels <Kierkels.r@gmail.com>
2020-01-12 20:24:35 +0100

    add tests for creating a message

--- apps/api/src/app/message/message.controller.spec.ts
+++ apps/api/src/app/message/message.controller.spec.ts
@@ -1,8 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MessageController } from './message.controller';
 import { MessageService } from './message.service';
+import { of } from 'rxjs';
+import { MessageCreateDto, MessageDto } from './message.model';
+import { MessageEntity } from '../database/database-entities';
 
 jest.mock('./message.service');
+
+const userId = 'user1';
+const messageCreateDto: MessageCreateDto = {
+  ideaId: 'idea1',
+  text: 'Fake Message'
+};
+const messageEntity: Partial<MessageEntity> = {
+  id: '1',
+  text: messageCreateDto.text,
+  author: { id: userId, username: '', picture: '', ideas: [], messages: [] },
+  createdAt: new Date()
+};
 
 describe('Message Controller', () => {
   let controller: MessageController;
@@ -18,7 +33,30 @@ describe('Message Controller', () => {
     service = module.get(MessageService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('While creating a message', () => {
+    beforeEach(() => {
+      service.create$.mockReturnValueOnce(of(messageEntity as MessageEntity));
+    });
+
+    it('should call the message service', () => {
+      controller.create(messageCreateDto, userId).subscribe({
+        next: () => {
+          expect(service.create$).toHaveBeenCalledTimes(1);
+          expect(service.create$).toHaveBeenCalledWith(
+            messageCreateDto,
+            userId
+          );
+        },
+        error: () => fail()
+      });
+    });
+
+    it('should map the created message to a DTO', () => {
+      controller.create(messageCreateDto, userId).subscribe({
+        next: actualMessage =>
+          expect(actualMessage instanceof MessageDto).toBeTruthy(),
+        error: () => fail()
+      });
+    });
   });
 });

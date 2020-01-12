6fd13c6c7156e966f7a516c71b3132c4e2a9bdad
Rik Kierkels <Kierkels.r@gmail.com>
2020-01-12 13:10:20 +0100

    create the test setup for the message controller

--- apps/api/src/app/message/message.controller.spec.ts
+++ apps/api/src/app/message/message.controller.spec.ts
@@ -1,15 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MessageController } from './message.controller';
+import { MessageService } from './message.service';
+
+jest.mock('./message.service');
 
 describe('Message Controller', () => {
   let controller: MessageController;
+  let service: jest.Mocked<MessageService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [MessageController]
+      controllers: [MessageController],
+      providers: [MessageService]
     }).compile();
 
-    controller = module.get<MessageController>(MessageController);
+    controller = module.get(MessageController);
+    service = module.get(MessageService);
   });
 
   it('should be defined', () => {

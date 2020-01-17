a288737f68468e6bc67bd6265c25c456a575899e
Rik Kierkels <Kierkels.r@gmail.com>
2020-01-18 00:25:27 +0100

    no longer throwing exception from user decorator
    There is no need to throw an exception if the user id is missing because it will always be present at this step in the request.

--- apps/api/src/app/shared/decorators/user.decorator.ts
+++ apps/api/src/app/shared/decorators/user.decorator.ts
@@ -1,9 +1,5 @@
-import { BadRequestException, createParamDecorator } from '@nestjs/common';
+import { createParamDecorator } from '@nestjs/common';
 
 export const UserId = createParamDecorator((data, req) => {
-  if (!req.user || !req.user.sub) {
-    return new BadRequestException('UserId is missing from request.');
-  }
-
   return req.user.sub;
 });

4479e2c9495446ffc2a72af1c66404e82727c0e2
Rik Kierkels <Kierkels.r@gmail.com>
2020-01-19 21:04:19 +0100

    create the setup for the idea service tests

--- apps/api/src/app/idea/idea.service.spec.ts
+++ apps/api/src/app/idea/idea.service.spec.ts
@@ -1,18 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { IdeaService } from './idea.service';
+import { UserService } from '../user/user.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { IdeaEntity, MessageEntity } from '../database/database-entities';
+import { MockType, repositoryMockFactory } from '../database/mock-repository';
+import { Repository } from 'typeorm';
+import { TagService } from '../tag/tag.service';
+
+jest.mock('../user/user.service');
+jest.mock('../tag/tag.service');
 
 describe('IdeaService', () => {
-  let service: IdeaService;
+  let ideaService: IdeaService;
+  let userService: jest.Mocked<UserService>;
+  let tagService: jest.Mocked<TagService>;
+  let repository: MockType<Repository<MessageEntity>>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [IdeaService]
+      providers: [
+        IdeaService,
+        UserService,
+        TagService,
+        {
+          provide: getRepositoryToken(IdeaEntity),
+          useValue: repositoryMockFactory()
+        }
+      ]
     }).compile();
 
-    service = module.get<IdeaService>(IdeaService);
+    ideaService = module.get(IdeaService);
+    userService = module.get(UserService);
+    tagService = module.get(TagService);
+    repository = module.get(getRepositoryToken(IdeaEntity));
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(ideaService).toBeDefined();
   });
 });

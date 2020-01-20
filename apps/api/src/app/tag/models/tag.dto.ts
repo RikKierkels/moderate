492de698ca977cc15086b1ce424fbe2c74343512
Rik Kierkels <Kierkels.r@gmail.com>
2020-01-20 21:19:28 +0100

    remove unused constructors

--- apps/api/src/app/tag/models/tag.dto.ts
+++ apps/api/src/app/tag/models/tag.dto.ts
@@ -2,12 +2,6 @@ import { ApiProperty } from '@nestjs/swagger';
 import { Tag } from '@moderate/api-interfaces';
 
 export class TagDto implements Tag {
-  constructor(id: string, name: string, color: string) {
-    this.id = id;
-    this.name = name;
-    this.color = color;
-  }
-
   @ApiProperty()
   readonly id: string;
 
--- apps/api/src/app/user/models/user.dto.ts
+++ apps/api/src/app/user/models/user.dto.ts
@@ -2,12 +2,6 @@ import { User } from '@moderate/api-interfaces';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UserDto implements User {
-  constructor(id: string, username: string, picture: string) {
-    this.id = id;
-    this.username = username;
-    this.picture = picture;
-  }
-
   @ApiProperty()
   readonly id: string;
 

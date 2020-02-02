04108a565405ab9e22b4550bb6b8ac431ddfd805
Rik Kierkels <Kierkels.r@gmail.com>
2020-02-02 16:50:30 +0100

    rewrite the idea controller tests

--- apps/api/src/app/idea/idea.controller.spec.ts
+++ apps/api/src/app/idea/idea.controller.spec.ts
@@ -2,21 +2,12 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { IdeaController } from './idea.controller';
 import { IdeaService } from './idea.service';
 import { of } from 'rxjs';
-import { IdeaEntity } from '../database/database-entities';
-import {
-  makeIdea,
-  makeUser
-} from '../shared/test-helpers/make-entities.test-utils';
-import { onNext } from '../shared/test-helpers/subscribe.test-utils';
 import { IdeaCreateDto } from './models/idea-create.dto';
 import { IdeaUpdateDto } from './models/idea-update.dto';
+import { makeIdea } from '../shared/test-helpers/make-entities.test-utils';
+import * as faker from 'faker';
 
 jest.mock('./idea.service');
-
-const ideaEntities: IdeaEntity[] = [
-  makeIdea('1', 'Fake Title', 'Fake Description', 1, makeUser()),
-  makeIdea('2', 'Fake Title', 'Fake Description', 1, makeUser())
-];
 
 describe('idea Controller', () => {
   let controller: IdeaController;
@@ -32,111 +23,67 @@ describe('idea Controller', () => {
     service = module.get(IdeaService);
   });
 
-  describe('While fetching all ideas', () => {
-    beforeEach(() => {
-      service.findAll$.mockReturnValueOnce(of(ideaEntities));
-    });
+  it('should fetch all ideas', done => {
+    const ideas = [makeIdea(), makeIdea()];
+    service.findAll$.mockReturnValueOnce(of(ideas));
 
-    it('should return all ideas', done => {
-      controller.findAll().subscribe(
-        onNext(ideas => {
-          expect(ideas).toEqual(ideaEntities);
-          done();
-        })
-      );
+    controller.findAll().subscribe(response => {
+      expect(response).toEqual(ideas);
+      done();
     });
   });
 
-  describe('While fetching an idea by id', () => {
-    beforeEach(() => {
-      service.findById$.mockReturnValueOnce(of(ideaEntities[0]));
-    });
+  it('should fetch an idea by id', done => {
+    const idea = makeIdea();
+    service.findById$.mockReturnValueOnce(of(idea));
 
-    it('should call the service with the id', done => {
-      controller.find({ id: '1' }).subscribe(
-        onNext(() => {
-          expect(service.findById$).toHaveBeenCalledTimes(1);
-          expect(service.findById$).toHaveBeenCalledWith('1');
-          done();
-        })
-      );
-    });
-
-    it('should return the found idea', done => {
-      controller.find({ id: '1' }).subscribe(
-        onNext(idea => {
-          expect(idea).toEqual(ideaEntities[0]);
-          done();
-        })
-      );
+    controller.find({ id: idea.id }).subscribe(response => {
+      expect(response).toEqual(idea);
+      done();
     });
   });
 
-  describe('While creating an idea', () => {
-    let ideaCreateDto: IdeaCreateDto;
+  it('should create an idea', done => {
+    const idea = makeIdea();
+    const ideaCreateDto: IdeaCreateDto = {
+      title: idea.title,
+      difficulty: idea.difficulty,
+      description: idea.description,
+      tags: ['1']
+    };
+    service.create$.mockReturnValueOnce(of(idea));
 
-    beforeEach(() => {
-      ideaCreateDto = { title: 'Fake Idea' } as IdeaCreateDto;
-      service.create$.mockReturnValueOnce(of(ideaEntities[0]));
-    });
+    controller
+      .create(ideaCreateDto, faker.random.uuid())
+      .subscribe(response => {
+        expect(response).toEqual(idea);
+        done();
+      });
+  });
 
-    it('should call the service with the correct params', done => {
-      controller.create(ideaCreateDto, 'userId').subscribe(
-        onNext(() => {
-          expect(service.create$).toHaveBeenCalledTimes(1);
-          expect(service.create$).toHaveBeenCalledWith(ideaCreateDto, 'userId');
-          done();
-        })
-      );
-    });
+  it('should update an idea', done => {
+    const idea = makeIdea();
+    const ideaUpdateDto: IdeaUpdateDto = {
+      id: idea.id,
+      title: idea.title,
+      difficulty: idea.difficulty,
+      description: idea.description,
+      tags: ['1']
+    };
+    service.update$.mockReturnValueOnce(of(idea));
 
-    it('should return the created idea', done => {
-      controller.create(ideaCreateDto, 'userId').subscribe(
-        onNext(idea => {
-          expect(idea).toEqual(ideaEntities[0]);
-          done();
-        })
-      );
+    controller.update(ideaUpdateDto).subscribe(response => {
+      expect(response).toEqual(idea);
+      done();
     });
   });
 
-  describe('While updating an idea', () => {
-    let ideaUpdateDto: IdeaUpdateDto;
+  it('should delete an idea', () => {
+    const idea = makeIdea();
+    service.delete$.mockReturnValueOnce(of(idea));
 
-    beforeEach(() => {
-      ideaUpdateDto = { id: '1', title: 'Fake Idea' } as IdeaUpdateDto;
-      service.update$.mockReturnValueOnce(of(ideaEntities[0]));
-    });
+    controller.delete({ id: idea.id });
 
-    it('should call the service with the idea to update', done => {
-      controller.update(ideaUpdateDto).subscribe(
-        onNext(() => {
-          expect(service.update$).toHaveBeenCalledTimes(1);
-          expect(service.update$).toHaveBeenCalledWith(ideaUpdateDto);
-          done();
-        })
-      );
-    });
-
-    it('should return the updated idea', done => {
-      controller.update(ideaUpdateDto).subscribe(
-        onNext(idea => {
-          expect(idea).toEqual(ideaEntities[0]);
-          done();
-        })
-      );
-    });
-  });
-
-  describe('While deleting an idea', () => {
-    beforeEach(() => {
-      service.delete$.mockReturnValueOnce(of(ideaEntities[0]));
-      controller.delete({ id: '1' });
-    });
-
-    it('should delete the idea by id', () => {
-      expect(service.delete$).toHaveBeenCalledTimes(1);
-      expect(service.delete$).toHaveBeenCalledWith('1');
-    });
+    expect(service.delete$).toHaveBeenCalledWith(idea.id);
   });
 });

6bab6a72556d3bc31eb929e53b30d4127550aa01
RikKierkels <Kierkels.r@gmail.com>
2020-02-29 15:17:58 +0100

    remove all root files used by monorepo

--- apps/moderate/src/app/auth/user-profile.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface UserProfile {
-  readonly name: string;
-  readonly nickname: string;
-  readonly picture: string;
-  readonly sub: string;
-  readonly updated_at: string;
-}
--- apps/moderate/src/app/shared/environment.model.ts
+++
@@ -1,7 +0,0 @@
-export class Environment {
-  readonly PRODUCTION: boolean;
-  readonly API_BASE_URL: string;
-  readonly AUTH0_DOMAIN: string;
-  readonly AUTH0_CLIENT_ID: string;
-  readonly AUTH0_AUDIENCE: string;
-}

4010f24bc0452842813bc12a23d30741c7aa7078
RikKierkels <Kierkels.r@gmail.com>
2020-03-20 14:13:50 +0100

    apply styling to the creator and creation date

--- client-react/src/idea/IdeaGridItem.tsx
+++ client-react/src/idea/IdeaGridItem.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './IdeaGridItem.scss';
+import moment from 'moment';
 import { Idea } from '../shared/interfaces/idea.interface';
 import truncate from '../shared/utils/truncate';
 
 type IdeaGridItemProps = { idea: Idea };
 export default function IdeaGridItem({ idea }: IdeaGridItemProps): JSX.Element {
+  const createdAt = moment(idea.createdAt).format('MMMM DD');
+
   return (
     <Link to={`/ideas/${idea.id}`} className="idea-card">
-      <article>
+      <article className="idea-card-container">
         <h3 className="idea-card-title">{idea.title}</h3>
         <p className="idea-card-description">
           {truncate(idea.description, 50)}
         </p>
 
-        <span>
-          {idea.createdAt}
-          By
-          {idea.author.username}
-        </span>
+        <div className="idea-card-details-container">
+          <span className="idea-card-post">
+            Posted by {idea.author.username}
+          </span>
+          <span className="idea-card-date">{createdAt}</span>
+        </div>
       </article>
     </Link>
   );
